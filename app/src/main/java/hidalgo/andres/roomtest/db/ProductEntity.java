8c6146e5b6f481dff622c071e3abad0306e94317
Andres Hidalgo <andreshidalgo08@gmail.com>
2018-01-21 17:26:12 -0400

    Created product entity

---
+++ app/src/main/java/hidalgo/andres/roomtest/db/ProductEntity.java
@@ -0,0 +1,59 @@
+package hidalgo.andres.roomtest.db;
+
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+
+/**
+ * Created by Administrador on 21/01/2018.
+ */
+
+@Entity(tableName = "products")
+public class ProductEntity {
+    @PrimaryKey
+    private int id;
+    private String name;
+    private String description;
+    private int price;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public ProductEntity() {
+    }
+
+    public ProductEntity(int id, String name, String description, int price) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
+}
