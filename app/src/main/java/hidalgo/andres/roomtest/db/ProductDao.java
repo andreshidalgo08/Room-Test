e341b58772e0be5385235869421af3530381060b
Andres Hidalgo <andreshidalgo08@gmail.com>
2018-01-21 18:32:22 -0400

    Reordered methods and added update and delete product methods

--- app/src/main/java/hidalgo/andres/roomtest/db/ProductDao.java
+++ app/src/main/java/hidalgo/andres/roomtest/db/ProductDao.java
@@ -1,9 +1,11 @@
 package hidalgo.andres.roomtest.db;
 
 import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
 import android.arch.persistence.room.Insert;
 import android.arch.persistence.room.OnConflictStrategy;
 import android.arch.persistence.room.Query;
+import android.arch.persistence.room.Update;
 
 import java.util.List;
 
@@ -13,12 +15,18 @@ import java.util.List;
 
 @Dao
 public interface ProductDao {
-    @Query("SELECT * FROM products")
-    List<ProductEntity> loadAllProducts();
-
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     void insertAll(List<ProductEntity> products);
 
+    @Query("SELECT * FROM products")
+    List<ProductEntity> loadAllProducts();
+
     @Query("select * from products where id = :productId")
     ProductEntity loadProduct(int productId);
+
+    @Update
+    void updateProduct(ProductEntity product);
+
+    @Delete
+    void deleteProduct(ProductEntity product);
 }
